6f2880d820ed812fd9b07a295919b00a1432b399
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2009-04-16 00:32:56 -0400

    Beautify.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def nav_link(name)
-    haml_tag(:li, {:class => (controller.kind_of?("#{name.to_s.camelize}Controller".constantize) ? 'active' : 'inactive' )}) do
-      haml_concat link_to(name.to_s.titleize, self.send("#{name.to_s.tableize}_path"))
-    end
+    haml_tag(:li, link_to(name.to_s.titleize, self.send("#{name.to_s.tableize}_path")), {:class => (controller.kind_of?("#{name.to_s.camelize}Controller".constantize) ? 'active' : 'inactive' )})
   end
 end
